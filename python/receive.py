d09d3a4ed15cc24a179ac799ab54718c6dd52328
lexuanthinh <lexuanthinh211419@gmail.com>
2019-09-18 11:01:07 +0700

    .

--- python/receive.py
+++ python/receive.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import pika
 
+credentials = pika.PlainCredentials('thinhle', 'meomeo')
+
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host='171.244.51.228', port=5672, virtual_host='/', credentials=credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
--- python/send.py
+++ python/send.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import pika
 
+credentials = pika.PlainCredentials('thinhle', 'meomeo')
+
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host='171.244.51.228', port=5672, virtual_host='/', credentials=credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+channel.basic_publish(exchange='', routing_key='hello', body='meomeo')
 print(" [x] Sent 'Hello World!'")
 connection.close()
+ 
